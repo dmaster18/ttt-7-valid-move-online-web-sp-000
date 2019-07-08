301a4940589ab165d175fc87ee235e9f20d2148d
David Strauch <dmaster18@gmail.com>
2019-07-08 20:37:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,7 @@
 def valid_move?
   if index <= board.length && index >= 0 
     
+    
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
